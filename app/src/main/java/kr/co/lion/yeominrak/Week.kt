2bcfa005cc2ee5d57113b5e39f6f908df050ca54
Jun <ayz1070@gmail.com>
2024-02-08 16:12:51 +0900

    Week, Week Enum Class 작성

---
+++ app/src/main/java/kr/co/lion/yeominrak/Week.kt
@@ -0,0 +1,11 @@
+package kr.co.lion.yeominrak
+
+enum class Week(var str:String) {
+    WEEK_SUNDAY("일"),
+    WEEK_MONDAY("월"),
+    WEEK_TUESDAY("화"),
+    WEEK_WENDESDAY("수"),
+    WEEK_THURSDAY("목"),
+    WEEK_FRIDAY("금"),
+    WEEK_SATURDAY("토")
+}
